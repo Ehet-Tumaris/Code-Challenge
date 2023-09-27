3bb862f7a673440327b3901e93fe5a77beac21d1
Tumaris Ahat <tumarisahat@Tumariss-MacBook-Air-2.local>
2023-09-27 16:35:30 -0400

    added write file function

--- tests/cardGame.spec.ts
+++ tests/cardGame.spec.ts
@@ -1,5 +1,7 @@
 import { test, request } from '@playwright/test';
 import { CardGamePage } from '../pages/card-game.page';
+import fs from 'fs';
+import testData from '../fixture/testdata.json'
 
 test.describe('Card Game', async () => {
 
@@ -7,11 +9,14 @@ test.describe('Card Game', async () => {
         let apiContext = await request.newContext()
         let cardGamePage = new CardGamePage(page);
         await cardGamePage.confirmSiteIsUpByApi(apiContext);
-        await cardGamePage.confirmSiteIsUpByUI();
+        // await cardGamePage.confirmSiteIsUpByUI();
         let deckId = await cardGamePage.getNewDeck(apiContext);
         await cardGamePage.shuffleDeck(apiContext, deckId);
-        let results = await cardGamePage.dealCards(apiContext, 2, 3, deckId);
+        let results = await cardGamePage.dealCards(apiContext, 5, 3, deckId);
         cardGamePage.gameResult(results);
-    })
+        testData.deck_Id = deckId;
+        fs.writeFileSync('fixture/testdata.json',JSON.stringify(testData,null,2))
+        }
+    )
 
 })
